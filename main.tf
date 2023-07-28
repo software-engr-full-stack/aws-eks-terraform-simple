7e437c23b9c6d0222f0b6356d4b0259c6e02863a
software-engr-full-stack <software.engr.full.stack@localhost>
2023-07-27 22:07:37 -0700

    1) Fix race condition. 2) Change instance type to t2.small because t2.micro is too small.

--- main.tf
+++ main.tf
@@ -9,18 +9,6 @@ module "eks" {
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
@@ -28,7 +16,9 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "BOTTLEROCKET_x86_64"
-    instance_types = ["t2.micro"]
+
+    # t2.micro is too small. kubectl run test-pod --image=nginx won't run.
+    instance_types = ["t2.small"]
 
     attach_cluster_primary_security_group = true
   }
@@ -39,7 +29,9 @@ module "eks" {
       max_size     = 2
       desired_size = 1
 
-      instance_types = ["t2.micro"]
+      # t2.micro is too small. kubectl run test-pod --image=nginx won't run.
+      instance_types = ["t2.small"]
+
       capacity_type  = "SPOT"
 
       tags = {
@@ -49,4 +41,32 @@ module "eks" {
   }
 
   tags = local.tags
+}
+
+# See # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1801#issuecomment-1028736746
+# All three EKS add-on modules can be put inside the EKS module but
+# race conditions will occur when creating a cluster from scratch.
+resource "aws_eks_addon" "coredns" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "coredns"
+
+  resolve_conflicts = "OVERWRITE"
+
+  depends_on = [module.eks.eks_managed_node_groups]
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "kube-proxy"
+
+  resolve_conflicts = "OVERWRITE"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "vpc-cni"
+
+  resolve_conflicts = "OVERWRITE"
+
+  depends_on = [module.eks.eks_managed_node_groups]
 }
